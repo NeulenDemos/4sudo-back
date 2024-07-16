5068bc8d4aef3485258c28075587b07c77898a1a
Nazar Taran <nazar.taran.id@gmail.com>
2024-07-16 14:23:18 +0200

    Init demo

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,8 @@
 namespace App\Models;
 
 use Orchid\Platform\Models\User as Authenticatable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     /**
      * The attributes that are mass assignable.
@@ -13,14 +12,10 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'login',
         'name',
         'email',
         'password',
         'permissions',
-        'picture',
-        'rating',
-        'role'
     ];
 
     /**
@@ -40,15 +35,8 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $casts = [
-        'permissions' => 'array',
-        'email_verified_at' => 'datetime',
-        'login' => 'string',
-        'name' => 'string',
-        'email' => 'string',
-        'password' => 'string',
-        'picture' => 'integer',
-        'rating' => 'integer',
-        'role' => 'string'
+        'permissions'          => 'array',
+        'email_verified_at'    => 'datetime',
     ];
 
     /**
@@ -75,22 +63,4 @@ class User extends Authenticatable implements JWTSubject
         'updated_at',
         'created_at',
     ];
-
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
-    public static function isAdmin()
-    {
-        $user_id = auth()->user()->id;
-        $query = User::query();
-        $result = $query->where('id', '=', $user_id)->where('role', '=', 'admin')->get()->all();
-        if (!empty($result))
-            return true;
-        return false;
-    }
 }
